6911ec725490f6a19a1cec53da4ed682194376c6
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-10-09 15:14:29 +0330

    User registeration and tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,5 +1,9 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse, resolve
+from .forms import CustomUserCreationForm
+from .views import SignUpView
+
 
 class CustomUserTest(TestCase):
     def test_create_user(self):
@@ -27,3 +31,25 @@ class CustomUserTest(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+
+class SignUpTest(TestCase):
+    """Sign Up needs to be tested, but login and logout don't because they have built-in functions."""
+
+    def setUp(self) -> None:
+        self.response = self.client.get(reverse('signup'))
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'registration/signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'bluh bluh bluh')
+
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, CustomUserCreationForm)
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_sign_up(self):
+        view = resolve('/accounts/signup/')
+        self.assertEqual(view.func.__name__, SignUpView.as_view().__name__)
--- book/urls.py
+++ book/urls.py
@@ -18,6 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django Admin
     path('admin/', admin.site.urls),
-    path('', include('pages.urls'))
+    # User Management
+    path('accounts/', include('django.contrib.auth.urls')),
+    # Local apps
+    path('accounts/', include('accounts.urls')),
+    path('', include('pages.urls')),
 ]
