ddc2c77917ab354550eed3fb6dc851c39299b6fa
Mahdi Abbasi <abbasimahdi782@gmail.com>
2023-10-14 20:44:00 +0330

    Reviews App and tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse, resolve
 from .forms import CustomUserCreationForm
-from .views import SignUpView
 
 
 class CustomUserTest(TestCase):
--- books/tests.py
+++ books/tests.py
@@ -1,16 +1,25 @@
-# books/tests.py
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-from .models import Book
+from .models import Book, Review
 
 
 class BookTests(TestCase):
-
     def setUp(self):
+        self.user = get_user_model().objects.create_user(  # new
+            username='reviewuser',
+            email='reviewuser@email.com',
+            password='testpass123'
+        )
         self.book = Book.objects.create(
             title='Harry Potter',
             author='JK Rowling',
             price='25.00',
+        )
+        self.review = Review.objects.create(  # new
+            book=self.book,
+            author=self.user,
+            review='An excellent review',
         )
 
     def test_book_listing(self):
@@ -30,4 +39,5 @@ class BookTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'Harry Potter')
+        self.assertContains(response, 'An excellent review')  # new
         self.assertTemplateUsed(response, 'books/book_detail.html')
